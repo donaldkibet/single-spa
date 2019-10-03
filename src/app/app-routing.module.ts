9df4cc280fd5c52756a84b5ab78b9274da45e938
Donald Kibet <chelashawdonald@yahoo.com>
2019-10-03 16:23:42 +0300

    Implementing angular-singlespa for angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app',
+    component: AppComponent
+  },
+  {
+    path: '',
+    redirectTo: '/app',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: EmptyRouteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide : APP_BASE_HREF, useValue: '/'
+    }
+  ]
 })
 export class AppRoutingModule { }
